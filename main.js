34d28c4f44da7c75d4c4033d7b49428a09fefa83
wendraw <252798869@qq.com>
2020-07-13 23:14:26 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function create(Cls, attrs) {
+    console.log(arguments);
+}
+
+let component = <div id="a"/>;
+
+class Div {
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: "./main.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                {
+                  pragma: "create",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development",
+  optimization: {
+    minimize: false,
+  },
+};
